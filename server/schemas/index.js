50a64abc6243e796921ff7479d14c89de69c0b92
Mgamboa <marianellag400@gmail.com>
2022-11-08 12:39:16 -0600

    had to restart with files, errors keep showing up, and prefer to restart with everything.

--- server/schemas/index.js
+++
@@ -1,6 +0,0 @@
-const typeDefs = require('./typeDef');
-const resolvers = require('./resolvers');
-
-module.exports = { typeDefs, resolvers };
-
-// * `index.js`: Export your typeDefs and resolvers.
